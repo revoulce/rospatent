a97a6142a271ba27f3a4646c353309d53f80cbd5
revoulce <54038231+revoulce@users.noreply.github.com>
2022-10-14 15:11:59 +0300

    Первый коммит, добавление сплеш-экрана и иконки в лаунчере

---
+++ app/src/main/java/ru/gov/rospatent/searchplatform/SplashActivity.kt
@@ -0,0 +1,29 @@
+package ru.gov.rospatent.searchplatform
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
